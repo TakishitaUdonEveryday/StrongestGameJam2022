bc42c2b812f4549f812ccc567466a3bdd00d2fa8
MiyakoMiya-owo <kgw.miyako@gmail.com>
2022-04-28 18:49:22 +0900

    BaseProjectを追加しました

---
+++ SGJ2022_BaseProject/Assets/02_Game/Scripts/Manager/StageManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace SGJ
+{
+    public class StageManager : MonoBehaviour
+    {
+        [Label("ステージリスト"), SerializeField]
+        private string[] m_list;
+
+        private void Awake()
+        {
+            var data = GameDataManager.Instance.GameData;
+            if (data.MaxStage > m_list.Length)
+            {
+                GameDebug.LogWarning("登録ステージが不足しています");
+            }
+            else
+            {
+                GameDebug.LogWarning("登録ステージが多すぎます");
+            }
+            SceneManager.LoadScene(m_list[data.StageNum % m_list.Length], LoadSceneMode.Additive);
+        }
+    }
+}
