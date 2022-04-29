90df0e5807ba23f7c231fed8ce29aa74ae134163
MiyakoMiya-owo <kgw.miyako@gmail.com>
2022-04-29 21:16:20 +0900

    NPCキャラのモデルを配置しました

--- SGJ2022_BaseProject/Assets/02_Game/Scripts/Character/Enemy/KingEnemy.cs
+++ SGJ2022_BaseProject/Assets/02_Game/Scripts/Character/Enemy/KingEnemy.cs
@@ -7,6 +7,21 @@ namespace SGJ
     {
         private NpcManager m_npcManager = null;
 
+        [SerializeField]
+        private float m_coolTime = 5f;
+
+        [SerializeField]
+        private GameObject m_zombiePrefab = null;
+
+        [Label("感染数"), SerializeField]
+        private int m_infected = 3;
+
+        [Label("感染範囲"), SerializeField]
+        private float m_rad = 5f;
+
+        private float m_time = 0;
+
+
         private void Start()
         {
             m_npcManager = NpcManager.Instance;
@@ -14,7 +29,27 @@ namespace SGJ
 
         private void Update()
         {
-            
+            if(m_time / m_coolTime < 1f)
+            {
+                m_time += Time.deltaTime;
+            }
+            else
+            {
+                // 一番近い一般人を感染させる
+                for(int i = 0; i < m_infected; ++i)
+                {
+                    var npc = m_npcManager.GetNearPeople(transform, m_rad);
+                    if (npc)
+                    {
+                        m_npcManager.RemovePeople(npc);
+                        var zom = Instantiate(m_zombiePrefab);
+                        zom.transform.position = npc.transform.position;
+                        zom.transform.rotation = npc.transform.rotation;
+                        Destroy(npc);
+                    }
+                }
+                m_time = 0f;
+            }
         }
     }
 }
--- SGJ2022_BaseProject/Assets/02_Game/Scripts/Manager/NpcManager.cs
+++ SGJ2022_BaseProject/Assets/02_Game/Scripts/Manager/NpcManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace SGJ
 {
@@ -18,6 +19,8 @@ namespace SGJ
         [SerializeField]
         private float m_rad = 20f;
 
+        private List<GameObject> m_peoples = new List<GameObject>();
+
         private void Start()
         {
             Create();
@@ -31,6 +34,7 @@ namespace SGJ
                 var obj = Instantiate(m_peopleObj, transform);
                 var pos = new Vector3(Random.Range(-m_rad, m_rad), obj.transform.position.y, Random.Range(-m_rad, m_rad));
                 obj.transform.position = pos;
+                m_peoples.Add(obj);
                 ++count;
             }
 
@@ -38,6 +42,33 @@ namespace SGJ
             var king = Instantiate(m_kingEnemy, transform);
             var kpos = new Vector3(Random.Range(-m_rad / 2, m_rad / 2), king.transform.position.y, Random.Range(-m_rad / 2, m_rad / 2));
             king.transform.position = kpos;
+        }
+
+        /// <summary>
+        /// 指定距離内で一番近い
+        /// </summary>
+        /// <param name="trans"></param>
+        /// <returns></returns>
+        public GameObject GetNearPeople(Transform trans, float rad)
+        {
+            var min = 999f;
+            GameObject ans = null;
+            foreach (var p in m_peoples)
+            {
+                var dis = Mathf.Abs(Vector3.Distance(trans.position, p.transform.position));
+                if (dis < min && dis < rad)
+                {
+                    ans = p;
+                    min = dis;
+                }
+            }
+
+            return ans;
+        }
+
+        public void RemovePeople(GameObject obj)
+        {
+            m_peoples.Remove(obj);
         }
     }
 }
