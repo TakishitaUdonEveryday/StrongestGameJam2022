7b181f251d5beccc3e46313ee609146c77f46d33
hosokawa <kirisimanozomi@gmail.com>
2022-04-29 15:23:16 +0900

    攻撃コリジョン回り仮実装
    攻撃コリジョン作成
    プレイヤーベースクラス追加

---
+++ SGJ2022_BaseProject/Assets/02_Game/Scripts/AttackCollision/AttackCollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AttackCollision : AttackCollisionBase
+{
+
+
+    protected override void Update()
+    {
+        // base.Update();
+
+
+        // ŒÅ—L‚ÌˆÚ“®‚ª‚ ‚éê‡‚È‚Ç‚Í‚±‚±‚É‹LÚ
+
+    }
+
+
+}
--- SGJ2022_BaseProject/Assets/02_Game/Scripts/Character/Player.cs
+++ SGJ2022_BaseProject/Assets/02_Game/Scripts/Character/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace SGJ
 {
     // プレイヤー固有の関数（アイテム影響）などを記述
-    public class Player : MonoBehaviour
+    public class Player : CharacterBase
     {
     }
 }
